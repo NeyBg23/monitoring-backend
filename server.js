a39f74c5a25ba7cb2eada7b03a438099a0181bc6
NeyBg23 <mathicuenta23@gmail.com>
2025-11-14 03:38:30 -0500

    cors

--- server.js
+++ server.js
@@ -20,7 +20,15 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Health check
 app.get('/', (req, res) => {
