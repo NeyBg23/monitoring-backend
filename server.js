029bd802b44bf56fa65bc5593a4617716284d158
NeyBg23 <mathicuenta23@gmail.com>
2025-11-15 02:48:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,16 +20,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
+// Middleware para parsear JSON
 // Health check
 app.get('/', (req, res) => {
   res.json({ message: 'Levantamiento Backend - OK', status: 'running' });
